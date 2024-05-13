aa90da71005f35cb515e98f6433b4ffbb3350e57
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-05-13 15:31:02 -0400

    struct and other functions created

--- f1/f1.cpp
+++ f1/f1.cpp
@@ -6,7 +6,26 @@
 /* DESCRIPTION : Write a program that takes in information about flights and stores the       */
 /*               destination and date in an array of structs to be displayed.                 */
 /*============================================================================================*/
+#include <stdio.h>
+#include <stdlib.h>
+struct FlightInfo
+{
+	char* destination;
+	char* date;
+};
+int fillFlightInfo(FlightInfo* myFlight, char* destination, char* date);
+void printFlightInfo(FlightInfo* myFlight);
 int main(void)
 {
 	return 0;
+}
+
+int fillFlightInfo(FlightInfo* myFlight, char* destination, char* date)
+{
+	return 0;
+}
+
+void printFlightInfo(FlightInfo* myFlight)
+{
+
 }
