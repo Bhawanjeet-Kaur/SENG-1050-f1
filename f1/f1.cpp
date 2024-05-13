e23d60bc8c60fb65fdff989075534095bfd08d3d
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-05-13 15:16:57 -0400

    Created main function and header comment

---
+++ f1/f1.cpp
@@ -0,0 +1,12 @@
+/*============================================================================================*/
+/* FILE : f1.cpp                                                                              */
+/* PROJECT : SENG1050- focused assignment 1                                                   */
+/* PROGRAMMER : Bhawanjeet Kaur Gill                                                          */
+/* FIRST VERSION : 2024-05-13                                                                 */
+/* DESCRIPTION : Write a program that takes in information about flights and stores the       */
+/*               destination and date in an array of structs to be displayed.                 */
+/*============================================================================================*/
+int main(void)
+{
+	return 0;
+}
