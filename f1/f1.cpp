34b01b78c2961d585b7a1c74dcaa095e548a796d
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-05-13 15:49:23 -0400

    function's header comments

--- f1/f1.cpp
+++ f1/f1.cpp
@@ -8,6 +8,10 @@
 /*============================================================================================*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#pragma warning(disable: 4996)
+const int kMaxLoop = 10;
 struct FlightInfo
 {
 	char* destination;
@@ -17,14 +21,27 @@ int fillFlightInfo(FlightInfo* myFlight, char* destination, char* date);
 void printFlightInfo(FlightInfo* myFlight);
 int main(void)
 {
+	struct FlightInfo myFlight[kMaxLoop];
 	return 0;
 }
-
+/*============================================================================================*/
+/* NAME: fillFlightInfo()                                                                     */
+/* DESCRIPTION: This function takes three parameters, uses malloc to allocate dynamic memory  */
+/*              and if succesful, put those inputs in the struct varibale.                    */
+/* PARAMETERS: FlightInfo* myFlight, char* destination, char* date.                           */
+/* RETURNS: 1 if memory allocation failed, 0 if succesful.                                    */
+/*============================================================================================*/
 int fillFlightInfo(FlightInfo* myFlight, char* destination, char* date)
 {
 	return 0;
 }
-
+/*============================================================================================*/
+/* NAME: printFlightInfo()                                                                    */
+/* DESCRIPTION: This function takes one parameter, and displays the destination and date(the  */
+/*              fields) in a proper format.                                                   */
+/* PARAMETERS: FlightInfo* myFlight.                                                          */
+/* RETURNS: Nothing.                                                                          */
+/*============================================================================================*/
 void printFlightInfo(FlightInfo* myFlight)
 {
 
